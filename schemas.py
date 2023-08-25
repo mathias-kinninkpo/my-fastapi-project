3bad9f0f4445ab934578710c45e5f3395cc12d24
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-08-25 17:16:07 +0100

    modification du fichier schemas et ajout de class

--- schemas.py
+++ schemas.py
@@ -7,20 +7,32 @@ from datetime import datetime
 T = TypeVar('T')
 
 
-class ArticleSchema(BaseModel):
-    id: Optional[int]
+class ArticleSchemaBase(BaseModel):
     title: Optional[str] = None
     image: Optional[str] = None
     short_description: Optional[str] = None
     description: Optional[str] = None
     author: Optional[str] = None
-    is_public: Optional[bool] = None
-    created_at : Optional[datetime] = None
-    updated_at : Optional[datetime] = None
-    deleted_at : Optional[datetime] = None
+   
 
     class Config:
         orm_mode = True
+        from_attributes = True
+
+
+class ArticleSchema(ArticleSchemaBase):
+       
+     id : Optional[int] = None
+     
+     is_public: Optional[bool] = None
+
+class ArticleSchemaAll(ArticleSchemaBase):
+       
+     id : Optional[int] = None
+     is_public: Optional[bool] = None
+     created_at : Optional[T] = None
+     updated_at : Optional[T] = None
+     deleted_at : Optional[T] = None
 
 
 class Request(GenericModel, Generic[T]):
@@ -29,6 +41,10 @@ class Request(GenericModel, Generic[T]):
 
 class RequestArticle(BaseModel):
     parameter: ArticleSchema = Field(...)
+
+
+class RequestArticleGet(BaseModel):
+    parameter: ArticleSchemaBase = Field(...)
 
 
 class Response(GenericModel, Generic[T]):
