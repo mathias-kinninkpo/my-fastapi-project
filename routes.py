bce9d2c9e81741329d94c21d2ec7c15792ee50bf
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-08-25 17:15:25 +0100

    ajouts de routes

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Path
 from fastapi import Depends
 from config import SessionLocal
 from sqlalchemy.orm import Session
-from schemas import ArticleSchema, Request, Response, RequestArticle
+from schemas import ArticleSchemaAll, Request, Response, RequestArticle, RequestArticleGet
 
 import crud
 
@@ -18,23 +18,43 @@ def get_db():
 
 
 @router.post("/create")
-async def create_article_service(request: RequestArticle, db: Session = Depends(get_db)):
+async def create_article_service(request: RequestArticleGet, db: Session = Depends(get_db)):
     crud.create_article(db,article=request.parameter)
     return Response(status="Ok",
                     code="200",
                     message="Article created successfully").dict(exclude_none=True)
 
 
-@router.get("/")
-async def get_articles(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    _articles = crud.get_article(db, skip, limit)
+@router.get("/all")
+async def get_articles(db: Session = Depends(get_db)):
+    _articles = crud.get_article(db)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_articles)
 
-@router.get("/{id}")
+
+
+
+@router.get("/all/{id}")
 async def get_article(db: Session = Depends(get_db), id: int = 0):
     _articles = crud.get_article_by_id(db, id)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_articles)
+    if _articles is not None:
+        return Response(status="Ok", code="200", message="Success fetch all data", result= ArticleSchemaAll.from_orm(_articles))
+    
+    raise HTTPException(status_code=404, message="Article not found")
 
+
+
+
+@router.get("/public")
+async def get_public_articles(db: Session = Depends(get_db)):
+    _articles = crud.get_public_article(db)
+    return Response(status="Ok", code="200", message="Success feth all public article", result=_articles)
+
+
+
+@router.get("/private")
+async def get_private_articles(db: Session = Depends(get_db)):
+    _articles = crud.get_private_article(db)
+    return Response(status="Ok", code="200", message="Success feth all private article", result=_articles)
 
 @router.patch("/update")
 async def update_article(request: RequestArticle, db: Session = Depends(get_db)):
@@ -42,7 +62,12 @@ async def update_article(request: RequestArticle, db: Session = Depends(get_db))
     return Response(status="Ok", code="200", message="Success update data", result=_article)
 
 
+
+
 @router.delete("/delete/{id}")
 async def delete_article(db: Session = Depends(get_db), id : int = None):
     crud.remove_article(db,id=id)
     return Response(status="Ok", code="200", message="Success delete data").dict(exclude_none=True)
+
+
+
