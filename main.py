96233ec20f3018f6501dda07b9d778265ef89cd9
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-08-28 20:09:36 +0100

    ajout des routes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import models
-from routes import router
+from routes import router, router1
 from config import engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -8,4 +8,5 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(router, tags=["Les actions sur les articles"])
+app.include_router(router1, tags=["Les actions sur les utilisateurs"])
 
